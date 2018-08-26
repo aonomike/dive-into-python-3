fa240579ef8e103149e3d2695295fdc155e9eef7
aonomike <aonomike@gmail.com>
2018-08-27 01:19:04 +0300

    Complete advanced iterators, start unittesting topics

--- generators/plural_5.py
+++ generators/plural_5.py
@@ -4,6 +4,7 @@ def rules(rules_filename):
             pattern, search, replace = line.split(None, 3)
             yield build_match_and_apply_functions(pattern, search, replace)
 
+
 def plural(noun, rules_filename="plural4-rules.txt"):
     for matches_rule, apply_rule in rules(filename):
         if matches_rule(noun):
